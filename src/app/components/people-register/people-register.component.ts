e090a7aca914d80b86690a9088fc26d1ba68a6cf
Cristian David Castro Montes <castrom1998@gmail.com>
2019-10-17 15:38:10 -0500

    Adding TipoDocumento to People-Registrer component

--- src/app/components/people-register/people-register.component.ts
+++ src/app/components/people-register/people-register.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Persona } from 'src/app/model/persona';
+import {TipoDocumento} from 'src/app/model/tipoDocumento'
+import {PersonaService} from 'src/app/service/persona-service.service'
+import {TipoDocumentoService} from 'src/app/service/TipoDocumento-service.service'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-people-register',
@@ -7,9 +12,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeopleRegisterComponent implements OnInit {
 
-  constructor() { }
+  public persona: Persona = new Persona();
+  public tipoDocumentos: TipoDocumento[];
+
+  constructor(private personaService: PersonaService, private tipoDocumentoService: TipoDocumentoService, private router: Router) { }
 
   ngOnInit() {
+    this.tipoDocumentoService.listarDocumentos().subscribe(
+      (tipoDocumentos) => {
+        this.tipoDocumentos = tipoDocumentos;
+      }
+    );
+  }
+
+  insert(){
+      this.personaService.insert(this.persona).subscribe(
+           _ => {
+             this.router.navigate(["/listar"]);
+           }
+      )
   }
 
 }
--- src/app/util/endpoints.ts
+++ src/app/util/endpoints.ts
@@ -1,4 +1,6 @@
 export class Endpoints {
-  public static readonly LISTAR="https://private-918f43-cridacamo.apiary-mock.com/personas";
+  public static readonly LISTAR="http://localhost:8080/personas/personas";
   public static readonly INSERTAR="http://localhost:8181/personas/persona";
+  public static readonly  LISTAR_DOCUMENTOS ="http://localhost:8080/TipoDocumento/TiposDocumentos";
+
 }
